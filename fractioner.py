cb071bac8d8e8232f2edcdf079445b4edc33cc44
alespecka <alespecka1@gmail.com>
2021-11-28 13:52:18 +0100

    Polish fractioner module and add 'help' command

--- fractioner.py
+++ fractioner.py
@@ -1,11 +1,33 @@
 import argparse
 
-from evaluation import evaluate, approxEvaluate
+import evaluation
 from input_error import InputError
+from fraction import Fraction
 
 
 symbol = "? "
 eps = 1e-12
+
+"""Command-line program that symbolically evaluates expressions with fractions."""
+
+
+helpString = "this is help string"
+
+
+def evalExpression(expression: str, testMode: bool = False) -> Fraction:
+	ans = evaluation.evaluate(expression)
+	print(ans)
+
+	if testMode:
+		approx = evaluation.approxEvaluate(expression)
+		error = abs(float(ans) - approx)
+		print(f"symbolic solution: {float(ans)}, numeric solution: {approx}, absolute error: {error}")
+		if error < eps:
+			print("TEST SUCCEEDED")
+		else:
+			print("TEST FAILED")
+
+	return ans
 
 
 def main() -> None:
@@ -15,38 +37,41 @@ def main() -> None:
 	testMode = args.testMode
 
 	ans = None
-	expression = input(symbol)
 
-	while expression.strip().lower() != "exit":
-		try:
-			ans = evaluate(expression)
-			print(ans)
-			if testMode:
-				approx = approxEvaluate(expression)
-				error = abs(float(ans) - approx)
-				print(f"symbolic solution: {float(ans)}, numeric solution: {approx}, absolute error: {error}")
-				if error < eps:
-					print("TEST SUCCEEDED")
-				else:
-					print("TEST FAILED")
-
-		except InputError as err:
-			if err.span:
-				begin = err.span[0] + len(symbol)
-				length = err.span[1] - err.span[0]
-				print(" " * begin + "^" * length)
-			print(err)
-		except ZeroDivisionError as err:
-			print(err)
-
+	while True:
 		expression = input(symbol)
-		if ans:
-			expression = expression.replace("ans", str(ans))
+
+		cmd = expression.strip().lower()
+		if cmd == "exit":
+			break
+		elif cmd == "help":
+			print(helpString)
+			continue
+		else:
+			try:
+				if ans:
+					expression = expression.replace("ans", str(ans))
+				else:
+					idx = expression.find("ans")
+					if idx >= 0:
+						raise InputError("variable 'ans' has not been set yet", span=(idx, idx+3))
+
+				ans = evalExpression(expression, testMode)
+			except InputError as err:
+				if err.span:
+					print(expression)
+					begin = err.span[0]
+					length = err.span[1] - err.span[0]
+					print(" " * begin + "^" * length)
+				print(err)
+			except ZeroDivisionError as err:
+				print(err)
 
 
 def testMain() -> None:
-	expression = "1 - 16.0 * 3"
-	# expression = "1/-2"
+	# expression = "1 - 16.0 * 3"
+	# expression = "-1/3"
+	expression = "-1/3"
 	# expression = "(-2 * (1 + 1_1/2) - 3/4) "
 	# expression = "3 * 1_1/2 - 3_4/5"
 	# expression = "(1_1/4 * 4 + 5) * 1/3"
@@ -55,12 +80,13 @@ def testMain() -> None:
 	try:
 		print(expression)
 
-		ans = evaluate(expression)
-		approx = approxEvaluate(expression)
+		ans = evaluation.evaluate(expression)
+		approx = evaluation.approxEvaluate(expression)
 
 		error = abs(float(ans) - approx)
 
 		print(f"ans = {ans}")
+		print(f"ans = {float(ans)}")
 		print(f"test = {approx}")
 		print(f"absolute error = {error}")
 	except InputError as err:
